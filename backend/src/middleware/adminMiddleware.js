860c265b76a916f5f116b34ba3d425b334703177
Sanjeevani <sanjeevani2710@gmail.com>
2026-07-07 16:20:59 +0530

    Complete authentication and authorization module

---
+++ backend/src/middleware/adminMiddleware.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+const pool = require("../config/db");
+
+async function isAdminLoggedIn(req, res, next) {
+  try {
+    let user = await pool.query("SELECT role FROM users WHERE id = $1", [
+      req.user.id,
+    ]);
+    if (user.rows.length === 0 || user.rows[0].role !== "admin") {
+      return res.status(403).json({ message: "Unauthorized" });
+    }
+    next();
+  } catch (err) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+}
+module.exports = isAdminLoggedIn;
