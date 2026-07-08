d1843a944c8a3794833018fed40cc2f1e8304daa
Sanjeevani <sanjeevani2710@gmail.com>
2026-07-08 19:29:20 +0530

    fix: import getProblem controller in problem routes

--- backend/src/routes/problemRoutes.js
+++ backend/src/routes/problemRoutes.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { isLoggedIn } = require("../middleware/authMiddleware");
 const { isAdminLoggedIn } = require("../middleware/adminMiddleware");
-const { createProblem , getAllProblems} = require("../controllers/problemController");
+const {
+  createProblem,
+  getAllProblems,
+  getProblem,
+} = require("../controllers/problemController");
 const { createTestCase } = require("../controllers/testCaseController");
 
 router.post("/", isLoggedIn, isAdminLoggedIn, createProblem);
@@ -12,7 +16,7 @@ router.post(
   isAdminLoggedIn,
   createTestCase,
 );
-router.get("/" , isLoggedIn , getAllProblems);
-router.get("/:id" , isLoggedIn , getProblem);
+router.get("/", isLoggedIn, getAllProblems);
+router.get("/:id", isLoggedIn, getProblem);
 
 module.exports = router;
