26f728046d4853925375b4f7c0a564fdb3e05ab5
Sanjeevani <sanjeevani2710@gmail.com>
2026-07-20 02:08:14 +0530

    updated outputComparator

--- backend/src/utils/outputComparator.js
+++ backend/src/utils/outputComparator.js
@@ -1,6 +1,12 @@
 const compareOutput = (actualOutput, expectedOutput) => {
-  actualOutput = actualOutput.trim();
-  expectedOutput = expectedOutput.trim();
-  return actualOutput === expectedOutput;
+  const normalize = (str) => {
+    return str.trim().replace(/\s+/g, " ").trim();
+  };
+
+  const normalizedActual = normalize(actualOutput);
+  const normalizedExpected = normalize(expectedOutput);
+
+  return normalizedActual === normalizedExpected;
 };
+
 module.exports = { compareOutput };
